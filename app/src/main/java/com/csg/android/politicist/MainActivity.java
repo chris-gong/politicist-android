3cd097fb31143d5cd9ae0f483311619e2e78aff1
Ron <ron@ekushkuley.com>
2017-10-29 23:59:48 -0400

    stud

--- app/src/main/java/com/csg/android/politicist/MainActivity.java
+++ app/src/main/java/com/csg/android/politicist/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int a;
-    //goon squad
+    //stud
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
