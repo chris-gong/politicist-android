03ba37e6c2888cbbffc89a7b9fdb202f5db66f38
Ron <ron@ekushkuley.com>
2017-10-29 23:54:37 -0400

    Merge branch 'master' of C:\Users\ron\Desktop\Politicist\politicist-android-master\politicist-android-master with conflicts.

--- app/src/main/java/com/csg/android/politicist/MainActivity.java
+++ app/src/main/java/com/csg/android/politicist/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    int a;
 
+    int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
