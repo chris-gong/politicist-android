ccbc021bf9f875512ff2f2e16d0f545bf193a89d
chris-gong <chris_gong@yahoo.com>
2017-11-05 23:18:14 -0500

    Made button go to another activity

--- app/src/main/java/com/csg/android/politicist/MainActivity.java
+++ app/src/main/java/com/csg/android/politicist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.csg.android.politicist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         playButton = (Button) findViewById(R.id.play_button);
-
+        playButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
