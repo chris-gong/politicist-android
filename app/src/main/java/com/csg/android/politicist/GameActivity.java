2272626efc4c9380a2b70b5be2c12c6777179e80
chris-gong <chris_gong@yahoo.com>
2017-11-05 23:36:48 -0500

    Changed layout of GameActivity and added a view pager. Also, changed Activity type to FragmentActivity

--- app/src/main/java/com/csg/android/politicist/GameActivity.java
+++ app/src/main/java/com/csg/android/politicist/GameActivity.java
@@ -1,13 +1,17 @@
 package com.csg.android.politicist;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends FragmentActivity {
 
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
     }
 }
